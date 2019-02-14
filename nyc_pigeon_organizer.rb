6112b3fb9e8320125490c394e42f8502c7a8fa87
Logan Wohlers <lawohlers@gmail.com>
2019-02-14 04:26:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newh={}
+  data.each do |k,v|
+    v.each do |k2, v2|
+      count=0
+      v2.each do |name|
+        if !newh.has_key?(v2[count])
+          newh[v2[count]]={}
+          newh[v2[count]][k]=[k2.to_s]
+        elsif !newh[v2[count]].has_key?(k)
+          newh[v2[count]][k]=[k2.to_s]
+        else
+          newh[v2[count]][k].push(k2.to_s)
+        end
+        count+=1
+      end
+    end
+  end
+  newh
 end
